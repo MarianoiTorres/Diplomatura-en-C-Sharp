35e37a60f1268c7cfa4987fcadce6c0686a094fe
MarianoiTorres <marianxtorres@gmail.com>
2025-03-18 01:14:08 -0300

    TP3-POO

---
+++ TP3-POO/TP3MarianoTorres/Persona.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3MarianoTorres
+{
+    public class Persona
+    {
+        private string nombre;
+        private string email;
+        private decimal sueldo;
+        private string sexo;
+        private decimal peso;
+        private decimal altura;
+
+        public Persona(string nombre, decimal sueldo, string sexo, decimal peso, decimal altura)
+        {
+            this.nombre = nombre;
+            this.email = this.CrearEmail();
+            this.sueldo = sueldo;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public string GetNombre() => nombre;
+        public string GetEmail() => email;
+        public decimal GetSueldo() => sueldo;
+        public string GetSexo() => sexo;
+        public decimal GetPeso() => peso;
+        public decimal GetAltura() => altura;
+
+
+        private string CrearEmail()
+        {
+            return $"{this.nombre}@superempresa.com";
+        }
+    }
+}
---
+++ TP3-POO/TP3MarianoTorres/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using TP3MarianoTorres;
+
+Console.WriteLine("Ingrese los datos de la persona:");
+Console.WriteLine("Nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Sueldo:");
+decimal sueldo = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Sexo:");
+string sexo = Console.ReadLine();
+Console.WriteLine("Peso:");
+decimal peso = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Altura:");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+Console.Clear();
+
+Persona unaPersona = new Persona(nombre, sueldo, sexo, peso, altura);
+
+Console.WriteLine("Nueva persona creada en el sistema:");
+Console.WriteLine($"Nombre: {unaPersona.GetNombre()}");
+Console.WriteLine($"Email: {unaPersona.GetEmail()}");
+Console.WriteLine($"Sueldo: {unaPersona.GetNombre()} ARS");
+Console.WriteLine($"Sexo: {unaPersona.GetNombre()}");
+Console.WriteLine($"Peso: {unaPersona.GetNombre()} kg(B)");
+Console.WriteLine($"Altura: {unaPersona.GetAltura() / 100} m");
+
+Console.ReadLine();
